2e52bfdb6d6fa06d08c13f118c69fb961b3dfc50
Eduarda Santana <eduarda.santanasb@gmail.com>
2021-11-29 11:58:38 -0300

    array-2-dimensoes

---
+++ lista-2-dimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ['José', 'George','Marina', 'Felipe', 'Elisa']
+
+const mediasAlunos = [7.5, 10, 8, 9.5, 6]
+
+let listaNotaseAlunos = [alunos, mediasAlunos]
+
+console.log(`${listaNotaseAlunos [0] [0]}, sua nota é ${listaNotaseAlunos [1] [0]}`)
