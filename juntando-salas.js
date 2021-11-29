14559f9dd4d4306d1d5aa29d665a8f32fe388256
Eduarda Santana <eduarda.santanasb@gmail.com>
2021-11-29 11:42:10 -0300

    junta-salas

---
+++ juntando-salas.js
@@ -0,0 +1,8 @@
+
+const salaPython = ['Laryssa', 'Katarina', 'Fernanda']
+
+const salaJava = ['Gabi', 'Nanda', 'Nany']
+
+const salasUnificadas = salaJava.concat(salaPython)
+
+console.log(salasUnificadas)
